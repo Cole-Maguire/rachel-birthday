bc9cbf97bdcec971090f32203f8764d4f333f453
Cole Maguire <hello@colemaguire.me>
2025-03-01 13:59:08 +1300

    intial commit with a homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,64 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="h-vh flex grid min-h-screen grid-flow-row gap-4 overflow-hidden p-8 font-[family-name:var(--bumble-font)]">
+      <header className="flex w-full min-w-min shrink justify-between">
+        <div>
+          <Image
+            className="dark:invert"
+            src="/rachel-birthday/bumble-logo.png"
+            alt="bumble logo"
+            width={100}
+            height={24}
+            priority
+          />
+        </div>
+        <span className="material-icons">tune</span>
+      </header>
+      <main className="h-9/10 bg-(--bumble-yellow) w-full flex-1 overflow-scroll rounded-lg">
+        <div className="h-vh min-h-min w-full flex-1 flex-col items-center gap-8 rounded-lg sm:items-start">
+          <div>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/rachel-birthday/blurred-main.png"
+              alt="My face"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="h-full w-full overflow-hidden rounded-lg object-cover"
+              priority
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="relative -mt-24 mb-4 px-4 text-white">
+              <div className="font-bold">Cole Maguire, 28</div>
+              <div>He/Him</div>
+              <div>Software Developer</div>
+            </div>
+          </div>
+
+          <div className="p-4">
+            I&apos;m just a boy looking for a very special girl on her birthday
+          </div>
+          <Image
+            src="/rachel-birthday/back-fall.jpg"
+            alt="My face"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-full w-full overflow-hidden rounded-lg object-cover" // optional
+            priority
+          />
+          <div className="p-4">Do you think you could help me?</div>
+          <Image
+            src="/rachel-birthday/my-face.jpg"
+            alt="My face"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full overflow-hidden rounded-lg object-cover" // optional
+            priority
+          />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className="flex flex-wrap items-center justify-center gap-6"></footer>
     </div>
   );
 }
